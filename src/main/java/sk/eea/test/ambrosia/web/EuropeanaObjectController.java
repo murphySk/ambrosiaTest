20588f576b1aa8f623c577aa6ec83772c063b1ab
Maros Mracna <mrc@eea.sk>
2014-04-23 13:38:48 +0200

    jboss napojenie

--- src/main/java/sk/eea/test/ambrosia/web/EuropeanaObjectController.java
+++ src/main/java/sk/eea/test/ambrosia/web/EuropeanaObjectController.java
@@ -2,6 +2,7 @@ package sk.eea.test.ambrosia.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import sk.eea.test.ambrosia.services.EuropeanaDataFetchService;
@@ -12,11 +13,13 @@ import sk.eea.test.ambrosia.services.EuropeanaDataFetchService;
 @Controller
 public class EuropeanaObjectController {
 
-//    @Autowired
-//    private EuropeanaDataFetchService europeanaDataFetchService;
+    @Autowired
+    private EuropeanaDataFetchService europeanaDataFetchService;
 
-    @RequestMapping(value = "/allobjects", method = RequestMethod.GET)
-    public String showAllObjects() { return "allobjects"; }
+    @RequestMapping(value = "/allobjects", method = RequestMethod.POST)
+    public String showAllObjects() {
+        return "allobjects";
+    }
 
     @RequestMapping(value = "/detailedObject", method = RequestMethod.GET)
     public String handleRequestAllObjects() { return "detailedObject"; }
